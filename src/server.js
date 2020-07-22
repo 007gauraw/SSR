e6adb10aae79121e12a1477a4ec7be804f2e5db0
007gauraw <30732181+007gauraw@users.noreply.github.com>
2020-07-23 02:11:06 +0530

    updated server port

--- src/server.js
+++ src/server.js
@@ -1,84 +1,94 @@
-import { join } from "path";
-import express from "express";
-import React from "react";
-import { renderToString } from "react-dom/server";
-import { App } from "./app";
-import template from "./template";
-const axios = require("axios");
+import { join } from 'path';
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import {App}  from './app';
+import template from './template';
+const axios = require('axios');
 
 const server = express();
 
-server.use("/assets", express.static(join(__dirname, "assets")));
+server.use('/assets', express.static(join(__dirname, 'assets')));
 
-async function loadApp(req, res, pageNumber = 1) {
+async function loadApp(req, res, pageNumber = 1){
   try {
-    let response = await axios.get(
-      `https://hn.algolia.com/api/v1/search?page=${pageNumber}`
-    );
+    let response = await axios.get(`https://hn.algolia.com/api/v1/search?page=${pageNumber}`)
     const initialState = {
-      name: "gaurav",
-      columns: [
+      "name": "gaurav", columns: [
         {
-          label: "author",
-          field: "author",
-          sort: "asc",
+          label: 'author',
+          field: 'author',
+          sort: 'asc',
           width: 150
         },
         {
-          label: "comment_text",
-          field: "comment_text",
-          sort: "asc",
+
+          label: 'comment_text',
+          field: 'comment_text',
+          sort: 'asc',
           width: 270
         },
         {
-          label: "Vote",
-          field: "upVote",
-          sort: "asc",
+
+          label: 'Vote',
+          field: 'upVote',
+          sort: 'asc',
           width: 270
         },
-        {
-          label: "title",
-          field: "title",
-          sort: "asc",
+     {
+
+          label: 'title',
+          field: 'title',
+          sort: 'asc',
           width: 270
         }
+
       ],
       rows: response.data.hits,
-      pageNumber: pageNumber
+      pageNumber: pageNumber,
+      
     };
-    const appString = renderToString(<App {...initialState} />);
+    const appString = renderToString(
+      <App {...initialState} />
+    );
 
     res.send(
       template({
         body: appString,
-        title: "Hello World from the server",
+        title: 'Hello World from the server',
         initialState: JSON.stringify(initialState)
-      })
-    );
-  } catch (error) {
+      }));
+  }
+  catch (error) {
     console.log(error);
   }
 }
 
-server.get("/", async (req, res) => {
+server.get('/', async (req, res) => {
   loadApp(req, res);
+
 });
 
-server.get("/:pageNumber", async (req, res) => {
-  let pageNumber = parseInt(req.params.pageNumber);
-  if (!isNaN(pageNumber)) {
+
+server.get('/:pageNumber', async (req, res) => {
+  let pageNumber = parseInt(req.params.pageNumber)
+  if (!isNaN(pageNumber)){
     loadApp(req, res, pageNumber);
-  } else {
+  }else{
     loadApp(req, res);
   }
+  
+
 });
 
-server.get("/upVote", async (req, res) => {
-  console.log("voted");
+server.get('/upVote', async (req, res) => {
+  console.log("voted")
   res.send("done");
-});
-server.listen(8080);
+})
+
+var port = process.env.PORT || 8080;
+server.listen(port);
 
 /* eslint-disable no-console */
-console.log("listening on 8080");
+console.log('listening on 8080');
 /* eslint-enable no-console */
